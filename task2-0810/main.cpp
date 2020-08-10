814a8fc830103bde208babb45eea4c428fbbd227
ipipos56 <ipipos56@bk.ru>
2020-08-10 15:16:16 +0300

    task2 done
    finish variant of task2

--- task2-0810/main.cpp
+++ task2-0810/main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+double sq(double a)
+{
+    return a*a;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    double x1,y1,x2,y2,x3,y3;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+    double side1,side2,side3;
+    side1 = sqrt(sq(x1-x2) + sq(y1-y2));
+    side2 = sqrt(sq(x1-x3) + sq(y1-y3));
+    side3 = sqrt(sq(x2-x3) + sq(y2-y3));
+    if(side1 + side3 > side2 && side1 + side2 > side3 && side3 + side2 > side1)
+        cout<<abs(x1*y2+x2*y3+x3*y1 - x2*y1-x3*y2-x1*y3)/2;
+    else
+        cout<<"This triangle does not exist";
 }
